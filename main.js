a7cde29ad5193db094ef07363814ef56acc2298f
Dizzysch <victorwilbert@gmail.com>
2021-10-24 00:01:09 -0300

    using github api

---
+++ main.js
@@ -0,0 +1,34 @@
+const socialMediaLinks = {
+  github: 'victorschlindwein',
+  instagram: 'victorschlindwein',
+  twitter: 'ralf_stifs',
+  youtube: 'UCPSnTS9KSJb3Ww01ycDApVA',
+  facebook: 'victorschlindwein'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+    li.children[0].href = `https://${social}.com/${socialMediaLinks[social]}`
+
+    // alert(li.children[0].href)
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubInfos() {
+  const url = `https://api.github.com/users/${socialMediaLinks.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      ;(userName.textContent = data.name),
+        (userBio.textContent = data.bio),
+        (userLink.href = data.html_url),
+        (userLogin.textContent = data.login),
+        (userImage.src = data.avatar_url)
+    })
+}
+
+getGitHubInfos()
